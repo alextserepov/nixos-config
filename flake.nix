0cc91de6585161bb85f839ff2913f8d273a69f3a
Aleksandr Tserepov-Savolainen <aleksandr.tserepov-savolainen@unikie.com>
2026-02-16 17:11:02 +0200

    semi-working rpi-hsm needs building
    Signed-off-by: Aleksandr Tserepov-Savolainen <aleksandr.tserepov-savolainen@unikie.com>

--- flake.nix
+++ flake.nix
@@ -3,13 +3,16 @@
 
   inputs = {
     nixpkgs.url="github:NixOS/nixpkgs/nixos-25.11";
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+    pkcs11-proxy.url = "github:tiiuae/pkcs11-proxy";
+    pkcs11-proxy.flake = false;
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ...}:
+  outputs = { self, nixpkgs, home-manager, nixos-hardware, ...}@inputs:
     let
       mkHost = { hostname, system ? "x86_64-linux", username ? "alextserepov" }:
         nixpkgs.lib.nixosSystem {
@@ -38,11 +41,42 @@
             }
           ];
         };
+        overlayPkcs11ProxyTii = final: prev: {
+          pkcs11-proxy-tii = prev.stdenv.mkDerivation {
+            pname = "pkcs11-proxy";
+            version = "tiiuae-main";
+
+            src = inputs.pkcs11-proxy;
+
+            nativeBuildInputs = with prev; [ cmake pkg-config ];
+            buildInputs = with prev; [ openssl ];
+
+            cmakeFlags = [];
+
+            doCheck = false;
+
+            meta = with prev.lib; {
+              description = "TIIUAE fork of pkcs11-proxy";
+              homepage = "https://github.com/tiiuae/pkcs11-proxy";
+              platforms = platforms.linux;
+            };
+          };
+        };
     in
     {
       nixosConfigurations = {
         work = mkHost { hostname = "work"; };
         cpx62 = mkHost { hostname = "cpx62"; system = "x86_64-linux"; };
+        rpi-hsm = nixpkgs.lib.nixosSystem {
+          system = "aarch64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            ({ ... }: { nixpkgs.overlays = [ overlayPkcs11ProxyTii ]; })
+            nixos-hardware.nixosModules.raspberry-pi-4
+            "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+            ./hosts/rpi-hsm/configuration.nix
+          ];
+        };
       };
     };
 }
