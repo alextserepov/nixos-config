4b2bb236e229d595bdfff2bc4f6094d703d3a61e
Aleksandr Tserepov-Savolainen <aleksandr.tserepov-savolainen@unikie.com>
2026-02-18 23:27:10 +0200

    arm-builder: add user, root SSH keys, and nix settings

--- hosts/arm-builder/configuration.nix
+++ hosts/arm-builder/configuration.nix
@@ -11,5 +11,30 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
 
+  users.users.alextserepov = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBOQxe0N4f5NcLYVyUrhh7jw+SqS1HxcrFDdZ1BLukgU aleksandr.tserepov-savolainen@unikie.com"
+    ];
+  };
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBOQxe0N4f5NcLYVyUrhh7jw+SqS1HxcrFDdZ1BLukgU aleksandr.tserepov-savolainen@unikie.com"
+  ];
+
+  nix.settings = {
+    "max-jobs" = "auto";
+    cores = 0;
+    trusted-users = [ "root" "alextserepov" ];
+    allowed-users = [ "root" "alextserepov" ];
+  };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
+
   system.stateVersion = "25.11";
 }
